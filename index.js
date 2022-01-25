af2e7bec4103c673695d59f915bf393319050647
Abhishek Maurya <abhishek@9stacks.com>
2022-01-26 04:56:50 +0530

    default port changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { getData } from './helpers.mjs';
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/getCases', async (req, res) => {
     try {
