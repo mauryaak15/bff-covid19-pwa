99e6d4e80a8da2da73c16b6ab4d6acf639b0f561
Abhishek Maurya <abhishek@9stacks.com>
2022-01-26 09:12:24 +0530

    cors support added

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import { getData } from './helpers.mjs';
 import express from 'express';
+import cors from 'cors';
+
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/getCases', async (req, res) => {
+app.get('/getCases', cors(corsOptions), async (req, res) => {
     try {
         const data = await getData();
         res.json(data);
